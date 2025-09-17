766a5f3a08608c1413cab3534b6717979e7b2f0e
ricardo-guanilo <ricardo.guanilo.p@uni.pe>
2025-09-17 17:21:44 -0500

    Modificacion de primo.py

--- primo.py
+++ primo.py
@@ -1,11 +1,16 @@
-n = int(input('Ingrese número: ')) #ingrese el número a evaluar
-
-if n == 2: # se establece el resultado de 2, como un número exceptuado
-    print('es primo')
+try:
+    n = int(input('Ingrese número: ')) # Ingrese el número a evaluar
+except:
+    print('ERROR: ingrese un número entero')
 else:
-    for i in range(2,n): # se evalua si es divisible por otros números además de 1 y el mismo: no primo
-        if n%i == 0:
-            print('no es primo')
-            break
-        if i == n-1: # si no, es divisible
-            print('es primo')
+    if n == 2: # Se establece el resultado de como caso especial
+        print('El número es primo')
+    elif n<=1:
+        print('ERROR: Ingrese un número mayor que 1')
+    else:
+        for i in range(2,n): # Se evalua si es divisible por otros números además de 1 y el mismo: no primo
+            if n%i == 0:
+                print('El número no es primo')
+                break
+            if i == n-1: # Si no, es divisible
+                print('El número es primo')
