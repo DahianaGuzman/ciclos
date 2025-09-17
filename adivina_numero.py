51db90a5979e0a5d4e67542b52b7ca74447636fe
B09S116est <B09S116est@MEDAULAS.UPB>
2025-09-17 16:39:55 -0500

    add: codigo numero secreto

---
+++ adivina_numero.py
@@ -0,0 +1,15 @@
+import random #libreria
+aleatorio = random.randint(1,100) #rango
+print("ADIVINA EL NÚMERO SECRETO")
+while True:
+  numUsuario=int(input("ingrese un número: "))
+  if numUsuario != aleatorio: #establecer número incorrecto
+      print("No haz adivinado el número secreto, intenta nuevamente :( ")
+      if numUsuario < aleatorio: #pistas para el usuario
+          print("El número secreto es mayor")
+      else:
+          print("El número secreto es menor")
+  else:
+      numUsuario == aleatorio #aviso de número correcto
+      print("¡Felicidades, haz adivinado el número secreto!")
+      break
